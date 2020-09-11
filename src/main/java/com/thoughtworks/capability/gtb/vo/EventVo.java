73b7f7d1f5f915522fc4dbb7d05aaf6a543ff041
lmhgithi <lmhfox@foxmail.com>
2020-09-11 14:00:59 +0800

    [lmh] finish problem1

--- src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/EventVo.java
@@ -1,6 +1,11 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +18,8 @@ public class EventVo {
   private String id;
   private String name;
   private EventType type;
+  @JsonFormat(pattern="yyyy-MM-dd hh:mm:ss",timezone="GMT+8")
   private Date time;
+  @JsonUnwrapped
   private UserVo user;
 }
