b15effbe0814c398afada847fa6387c0b9eb88ae
lmhgithi <lmhfox@foxmail.com>
2020-09-11 22:41:54 +0800

    [lmh] finish problem2

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.thoughtworks.capability.gtb.serializer.PersonSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,11 +12,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonSerialize(using = PersonSerializer.class)
 public class PersonVo {
   private String id;
   private Integer age;
   private String name;
   @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
+
+  public Integer getAge() {
+    if(this.age == null) {
+      return 0;
+    } else {
+      return this.age;
+    }
+  }
 }
