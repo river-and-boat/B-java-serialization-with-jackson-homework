d9f0a53c3e5e8ba19218bf125f8671771f0b71d0
RiverAndBoat <842714673@qq.com>
2020-09-14 11:41:42 +0800

    Update DataDeserializer.java
    为啥我不写反序列化器，前端传Long过来也可以自动解析呢。

--- src/main/java/com/thoughtworks/capability/gtb/serializer/DataDeserializer.java
+++ src/main/java/com/thoughtworks/capability/gtb/serializer/DataDeserializer.java
@@ -9,14 +9,14 @@ import java.io.IOException;
 import java.time.Instant;
 import java.util.Date;
 
-public class DataDeserializer extends StdDeserializer<Date> {
-    protected DataDeserializer() {
-        super(Date.class);
-    }
+// public class DataDeserializer extends StdDeserializer<Date> {
+//     protected DataDeserializer() {
+//         super(Date.class);
+//     }
 
-    @Override
-    public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        Instant instant = Instant.ofEpochMilli(p.getLongValue());
-        return Date.from(instant);
-    }
-}
+//     @Override
+//     public Date deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+//         Instant instant = Instant.ofEpochMilli(p.getLongValue());
+//         return Date.from(instant);
+//     }
+// }
